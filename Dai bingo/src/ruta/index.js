37490334890de4d20325bc83066a1020a3f2e1cb
ianBernard <82122990+ianBernard@users.noreply.github.com>
2022-05-10 20:19:17 -0300

    Update index.js

--- Dai bingo/src/ruta/index.js
+++ Dai bingo/src/ruta/index.js
@@ -1,6 +1,6 @@
 const{Router}=require('express');
 const router = Router();
-
+//Acta lo que hice fue tratar de mandar cosas a la API a ver si le llegaba la informacion adecuadamente
 router.get('/test', (req,res) => {
     const data= {
         "name": "ian",
